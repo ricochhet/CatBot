3705bb93cd0d9c790e988311efa415d606209ad5
John <55989519+ricochhet@users.noreply.github.com>
2019-10-01 12:11:38 -0500

    Added example command for CatBot's info command

---
+++ infoExample.js
@@ -0,0 +1,30 @@
+if(input == "itemname") {
+  const monsterEmbed = new Discord.RichEmbed()
+  .setColor('#8fde5d')
+  .setTitle("ItemName Title")
+  .setURL('Fextralife Wiki Link')
+  .setDescription("What Kind of Object")
+  .addField('Description', 'Description', true)
+  .addField('Locations', 'Where can it be found?')
+  .setTimestamp()
+  .setFooter('Info Menu');
+
+  message.channel.send(monsterEmbed);
+} else if(input == "itemname2") {
+  const monsterEmbed = new Discord.RichEmbed()
+  .setColor('#8fde5d')
+  .setTitle("ItemName Title 2")
+  .setURL('Fextralife Wiki Link')
+  .setDescription("What Kind of Object")
+  .addField('Description', 'Description', true)
+  .addField('Locations', 'Where can it be found?')
+  .setTimestamp()
+  .setFooter('Info Menu');
+
+  message.channel.send(monsterEmbed);
+}
+
+// continue with else if's like "itemname2"
+// Ancient Forest: Sector 1 and 8\n Wildspire Waste: Sector 1 - Example Layout of Locations - All Locations is All Locations
+
+// Add/remove fields as needed
