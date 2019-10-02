a44918cb2d98e055719ab923d66c2b5bf3b7404a
John <55989519+ricochhet@users.noreply.github.com>
2019-10-02 08:01:16 -0500

    Create armorInfoExample.js

---
+++ armorInfoExample.js
@@ -0,0 +1,48 @@
+/* -lr symbolizes low rank, -hr high rank, and -mr master rank */
+if(input == "alloy-lr") {
+  const monsterEmbed = new Discord.RichEmbed()
+  .setColor('#8fde5d')
+  .setTitle("Alloy Armor (Low Rank)")
+  .setURL('https://monsterhunterworld.wiki.fextralife.com/Alloy+Armor+Set')
+  .setDescription("Derived from Ore") /* Go to the armors page and pick something that seems interesting */
+  .setThumbnail('https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/alloy_armor_set_mhw_small.png') /* Icon can be found on the far right, shows both male and female variants*/
+  .addField('Stats', 'Defense: 40\n Fire Res: -10\n Water Res: 5\n Thunder Res: -10\n Ice Res: -10\n Dragon Res: 5', true) /* Optional, but recommended */
+  .addField('Skills', 'Blah', true) /* Optional, but recommended */
+  .addField('Materials', 'Zenny x 2500\n Machalite Ore x 17\n Iron Ore x 26\n Earth Crystal x 7\n') /* Optional, but recommended */
+  .setTimestamp()
+  .setFooter('Info Menu');
+
+  message.channel.send(monsterEmbed);
+} else if(input == "alloy-lr") { /*  DIFFERENT ARMOR SET, I just copied it with an else if because I'm lazy  */
+  const monsterEmbed = new Discord.RichEmbed()
+  .setColor('#8fde5d')
+  .setTitle("Alloy Armor (Low Rank)")
+  .setURL('https://monsterhunterworld.wiki.fextralife.com/Alloy+Armor+Set')
+  .setDescription("Derived from Ore") /* Go to the armors page and pick something that seems interesting */
+  .setThumbnail('https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/alloy_armor_set_mhw_small.png') /* Icon can be found on the far right, shows both male and female variants*/
+  .addField('Stats', 'Defense: 40\n Fire Res: -10\n Water Res: 5\n Thunder Res: -10\n Ice Res: -10\n Dragon Res: 5', true) /* Optional, but recommended */
+  .addField('Skills', 'Blah', true) /* Optional, but recommended */
+  .addField('Materials', 'Zenny x 2500\n Machalite Ore x 17\n Iron Ore x 26\n Earth Crystal x 7\n') /* Optional, but recommended */
+  .setTimestamp()
+  .setFooter('Info Menu');
+
+  message.channel.send(monsterEmbed);
+} else if(input == "alloy-lr") { /*  DIFFERENT ARMOR SET, I just copied it with an else if because I'm lazy  */
+  const monsterEmbed = new Discord.RichEmbed()
+  .setColor('#8fde5d')
+  .setTitle("Alloy Armor (Low Rank)")
+  .setURL('https://monsterhunterworld.wiki.fextralife.com/Alloy+Armor+Set')
+  .setDescription("Derived from Ore") /* Go to the armors page and pick something that seems interesting */
+  .setThumbnail('https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/alloy_armor_set_mhw_small.png') /* Icon can be found on the far right, shows both male and female variants*/
+  .addField('Stats', 'Defense: 40\n Fire Res: -10\n Water Res: 5\n Thunder Res: -10\n Ice Res: -10\n Dragon Res: 5', true) /* Optional, but recommended */
+  .addField('Skills', 'Blah', true) /* Optional, but recommended */
+  .addField('Materials', 'Zenny x 2500\n Machalite Ore x 17\n Iron Ore x 26\n Earth Crystal x 7\n') /* Optional, but recommended */
+  .setTimestamp()
+  .setFooter('Info Menu');
+
+  message.channel.send(monsterEmbed);
+}
+
+/* CONTINUE WITH **ELSE IF** STATEMENTS */
+
+/* If you find certain stats to be hard to read use "https://mhworld.kiranico.com/" but DON'T use the site links at all for it, use FEXTRALIFE */
