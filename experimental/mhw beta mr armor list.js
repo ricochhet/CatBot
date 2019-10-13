6c65e04af56675a10c625984c09a115d032bf459
John <55989519+ricochhet@users.noreply.github.com>
2019-10-13 16:29:27 -0500

    Removing outdated files

--- experimental/mhw beta mr armor list.js
+++
@@ -1,9 +0,0 @@
-if(command === "blah blah choose a name") {
-    const listEmbed = new Discord.RichEmbed()
-    setColor('#8fde5d')
-    addField("Beta Master Rank Armor List", "Alloy Beta + Armor Set\n Anja Beta + Armor Set\n Baan Beta + Armor Set\n Banbaro Beta + Armor Set\n Barroth Beta + Armor Set\n Beo Beta + Armor Set\n Bone Beta + Armor Set\n Dodogama Beta + Armor Set\n Girros Beta + Armor Set\n High Metal Beta + Armor Set\n Hornetaur Beta + Armor Set\n Jagras Beta + Armor Set\n Jyura Beta + Armor Set\n Kadachi Beta + Armor Set\n Viper Kadachi Beta + Armor Set\n Kulu Beta + Armor Set\n Lumu Beta + Armor Set\n Lumu Phamtasm Beta + Armor Set\n Pukei Beta + Armor Set\n Coral Pukei Beta + Armor Set\n Rath Heart Beta + Armor Set\n Rathian Beta + Armor Set\n Tzitzi Beta + Armor Set\n Vespoid Beta + Armor Set\n Kestodon Beta +\n Gajau Beta +\n Shamos Beta +\n Gastodon Beta +\n Barnos Beta +\n Wulg Beta +\n Cortos Beta +\n Artian Beta +\n Clockwork Beta +\n Ingot Beta +\n Barioth Beta +\n Nargacuga Beta +\n Glavenus Beta +\n Black Belt Beta +\n Brachydios Beta +\n Diablos Beta +\n Odogaron Beta +\n Uragaan Beta +\n Rath Soul Beta +\n Tigrex Beta +\n Rathalos Beta +\n Legiana Beta +\n Shrieking Legia Beta +\n Acidic Glavenus Beta +\n Damascus Beta +\n Death Garon Beta +\n Blackveil Hazak Beta +\n Diablos Nero Beta +\n Dober Beta +\n Fulgur Anja Beta +\n Kirin Beta +\n Kushala Beta +\n Lavasioth Beta +\n Namielle Betan+\n Savage Jho Beta +\n Seething Bazel Beta +\n Shara Ishvalda Beta +\n Teostra Beta +\n Velkhana Beta +\n Zorah Beta +\n Lunastra Beta +\n Yian Garuga Beta +\n Zinogre Beta +\n Ruiner Nergi Beta +\n Golden LUne Beta +\n Silver Sol Beta +\n Guild Palace Beta +")
-    setTimestamp()
-    setFooter('List Menu');
-
-    message.channel.send(listEmbed);
-}
