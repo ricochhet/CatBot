4289d56d1cd11a6f54901c7e9cef97d5e806ec34
John <55989519+ricochhet@users.noreply.github.com>
2019-10-13 16:25:58 -0500

    Delete commandNameExample.js

--- examples/commandNameExample.js
+++
@@ -1,9 +0,0 @@
-if(command === "commandName") {
-  const embed = new Discord.RichEmbed()
-  .setColor('#8fde5d')
-  .setImage("Example Embed, Don't Copy, use the other examples for correct example embeds")
-  .setTimestamp()
-  .setFooter('Example Menu');
-
-  message.channel.send(embed);
-}
