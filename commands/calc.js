6a4398ff0091873fc63e0d9412ff910ffd0a836f
Nathan <39814328+Yofou@users.noreply.github.com>
2019-10-22 04:37:58 +0100

    Fixed calc doubled posting embed & added affinity calc

--- commands/calc.js
+++ commands/calc.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 module.exports = {
   name: 'calc',
   args: false,
-  usage : 'calc <dmgtaken/elementalcalc/eraw/rawcalc> <args for dmgtaken/elementalcalc/eraw/rawcalc>',
+  usage : 'calc <dmgtaken/elementalcalc/eraw/rawcalc>\n<args for dmgtaken/elementalcalc/eraw/rawcalc>',
   error (message) {
     const data = [];
     data.push('\n**dmgtaken Args:**\n\n Defense (Number)\n');
     data.push('**elementalcalc Args:**\n\ndamage: base damage value\nsharpness (elemental): none, red, orange, yellow, green, blue, white, purple\nmonsterpartmultiplier: multiplier value\n');
     data.push('**eraw Args:**\n\ndamage: base damage value\nweapontype: bow, cb, db, gs, gl, hammer, hbg, hh, ig, lance, lbg, ls, sa, sns\n');
     data.push('**rawcalc Args:**\n\ndamage: base damage value\nweapontype: bow, cb, db, gs, gl, hammer, hbg, hh, ig, lance, lbg, ls, sa, sns\nsharpness (raw): none, red, orange, yellow, green, blue, white, purple\nmonsterpartmultiplier: multiplier value\n');
+    data.push('**affinitycalc**\n\naffinity: base affinity value\ndamage: base damage value')
 
     const usageEmbed = new Discord.RichEmbed()
       .setColor('#8fde5d')
@@ -28,24 +29,25 @@ module.exports = {
     args = args.slice(1,args.length)
 
     // Gets all Math commands from math folder
-    fs.readdir("./math", function(err, files) {
+    fs.readdir("./math", (err, files) => {
       if (err) return console.error(err);
-      files.forEach(function(file) {
+      let responce = false
+      files.forEach((file) => {
         if (!file.endsWith(".js")) return;
         let command = require(`../math/${file}`);
 
         // if subArg is command run it
         if (subArg == command.name){
-          return command.run(client, message, args)
+          responce = true
+          command.run(client, message, args)
         }
-
       });
-    });
 
-    // Checks if there were no arguments
-    if (args < 1) {
-      this.error(message)
-    }
+      // Checks if there were no arguments
+      if (!responce) {
+        this.error(message)
+      }
+    });
 
   }
 }
