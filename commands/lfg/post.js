ee971300553383eedca7206b9851d9123c96f771
Chad <chadiaw.dev@gmail.com>
2020-02-26 00:54:35 -0500

    refactor/fix lfg post, update help

--- commands/lfg/post.js
+++ commands/lfg/post.js
@@ -13,7 +13,7 @@ class Post extends Command {
     const data = [];
     data.push('platform: PC, PS4, or XBOX\n');
     data.push(
-      'session id: Must be between 11 and 13 characters long for PC; and between 14 and 16 characters long for console (including spaces)\n'
+      'session id:\n - Must be between 11 and 13 characters long for PC\n - `xxxx-xxxx-xxxx` or `xxxx xxxx xxxx` for PS4/XBOX\n'
     );
     data.push('description: Describe what you plan to do in the session\n');
     const embed = this.RichEmbed().setColor('#8fde5d');
@@ -27,7 +27,7 @@ class Post extends Command {
       .addField('Parameters Help', data.join('\n'))
       .addField(
         'Examples',
-        'PC: +lfg post pc u7Mpp4F8Z$Wh This is a test description\nConsole: +lfg post xbox 4ZjN zKwZ TCdX This is a test description'
+        `${this.prefix}lfg post pc u7Mpp4F8Z$Wh This is a test description\n${this.prefix}lfg post xbox 4ZjN zKwZ TCdX This is a test description`
       )
       .setTimestamp();
 
@@ -98,63 +98,20 @@ class Post extends Command {
   }
 
   async run(client, message, args) {
-    if (args.length == 1)
+    // Validate the arguments
+    if (args.length < 2)
       return message.channel.send(this.usageEmbed('Session ID is required'));
 
-    // load in the current posts from the json db
-    const posts = require('../../utils/databases/lfg/lfg.json');
-
-    const response = this.RichEmbed();
-
-    // Validate the arguments
-    let sessionID;
+    let sessionID, description;
     const platform = args[0].toLowerCase();
 
-    if (['ps4', 'xbox'].includes(platform)) {
-      // for console the format is 'xxxx xxxx xxxx'(need to join args)
+    if (!['pc', 'ps4', 'xbox'].includes(platform)) {
+      return message.channel.send(
+        this.usageEmbed(`${platform} is not valid platform.`)
+      );
+    }
 
-      if (args.length >= 4) {
-        let tempArr = [];
-        for (var i = 1; i < args.length; i++) {
-          if (args[i].length <= 4) {
-            tempArr.push(args[i]);
-            args.slice(i);
-          }
-        }
-
-        if (tempArr.length >= 1) {
-          sessionID = tempArr.join(' ');
-        } else {
-          return message.channel.send(
-            this.usageEmbed(`Can not find the session id`)
-          );
-        }
-      } else if (args.length >= 2) {
-        sessionID = args[1];
-      } else {
-        return message.channel.send(
-          this.usageEmbed(`Can not find the session id`)
-        );
-      }
-      if (
-        sessionID.length == 0 ||
-        sessionID.length < 14 ||
-        sessionID.length > 16
-      ) {
-        return message.channel.send(
-          this.usageEmbed(
-            `XBOX/PS4 session ids need to be between 14 and 16 characters long \`${sessionID}\` is only ${sessionID.length} characters long.`
-          )
-        );
-      }
-
-      if (sessionID.split(' ').length != 3 && sessionID.split(' ').length != 1)
-        return message.channel.send(
-          this.usageEmbed(
-            `XBOX/PS4 session ids needs to be in the format of xxxx xxxx xxxx or xxxx-xxxx-xxxx`
-          )
-        );
-    } else if (platform == 'pc') {
+    if (platform === 'pc') {
       sessionID = args[1];
 
       if (
@@ -164,15 +121,50 @@ class Post extends Command {
       ) {
         return message.channel.send(
           this.usageEmbed(
-            `PC session ids need to be between 11 and 13 characters long \`${sessionID}\` is only ${sessionID.length} characters long.`
+            `PC session ids must be between 11 and 13 characters long \`${sessionID}\` is ${sessionID.length} characters long.`
           )
         );
       }
-    } else {
+
+      // if args.length <= 2, desc will just be empty
+      description = args.slice(2, args.length).join(' ');
+    }
+
+    if (['ps4', 'xbox'].includes(platform)) {
+      if (args[1].includes('-')) {
+        // has dashes, can assume its the whole sessionID
+        sessionID = args[1];
+        description = args.slice(2, args.length).join(' ');
+      } else {
+        // no dashes, assume its spaces, grab next 3 (we check for length later)
+        sessionID = args.slice(1, 4).join(' ');
+        description = args.slice(4, args.length).join(' ');
+      }
+
+      if (!sessionID || sessionID.length !== 14) {
+        return message.channel.send(
+          this.usageEmbed(
+            `XBOX/PS4 session ids must be in the format of \`xxxx xxxx xxxx\` or \`xxxx-xxxx-xxxx\`.`
+            //`XBOX/PS4 session ids need to be between 14 and 16 characters long \`${sessionID}\` is ${sessionID.length} characters long.`
+          )
+        );
+      }
+    }
+
+    if (!description) {
+      description = 'No description provided.';
+    }
+
+    if (description.length > 256) {
       return message.channel.send(
-        this.usageEmbed(`${platform} is not valid platform.`)
+        this.usageEmbed('Description is larger than 256 characters.')
       );
     }
+
+    // load in the current posts from the json db
+    const posts = require('../../utils/databases/lfg/lfg.json');
+
+    const response = this.RichEmbed();
 
     // Checks if the sessionID has already been posted
     if (
@@ -186,21 +178,8 @@ class Post extends Command {
 
     // Create the new post
     const newPost = {
-      description: '' // make default empty string
+      description: description
     };
-
-    if (args.length > 2) {
-      if (['ps4', 'xbox'].includes(platform)) {
-        newPost['description'] = args.slice(4, args.length).join(' ');
-      } else {
-        newPost['description'] = args.slice(2, args.length).join(' ');
-      }
-
-      if (newPost['description'].length > 256)
-        return message.channel.send(
-          this.usageEmbed('Description is larger than 256 characters.')
-        );
-    }
 
     // Checks if the user has already posted or not
     let repost = false;
