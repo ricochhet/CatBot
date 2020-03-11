47843eb352ac45ff1001d193fdd04fe973398cb2
Ricochet <55989519+ricochhet@users.noreply.github.com>
2020-03-10 19:56:35 -0500

    Updated changelog, NOTE: changed config bot version to '2.1.0'

--- commands/main/changelog.js
+++ commands/main/changelog.js
@@ -14,17 +14,12 @@ class About extends Command {
       .addField(
         `Changelog: v${client.config.get('VERSION')}`,
         `
-        🤖 Now uses ES6; features can be pushed out faster!
-        🔍 Similarity algorithm improved, now you can find what you want much quicker!
-        🎲 \`+mhw rollhunt\` is now much cleaner and easier to understand, also shows weapon type along with name!
-        ⛔ The bot will notify you of what permissions it needs instead of not working properly!
-        🧠 You can now search for decorations by the skill name!
-        📖 Calculations are now more descriptive and useful towards hunts!
-        ❓ Sub-help menus are easier on the eyes, and easier to read!
-        ❓ Mentioning the bot now tells you how to get started!
-        🆕 Use \`+mhw list\` to get a list of monsters!
-        🆕 Use \`+mhw events\` to get a list of events!
-        🆕 Use \`+changelog\` to check out all of the new features!
+        🤖 Updated to latest version of DiscordJS.
+        📖 \`+calc eraw\` now has the option to not have any critical boost.
+        🆙 Iceborne weapons have been finally added to \`+mhw weapon\`!
+        🆕 Want to see how the bot is doing? Use \`+stats\` to see interesting information about the bot!
+        🆕 Finally, a bit of customization! Use \`+ignore [channel id / all]\` to have the bot ignore certain channels!
+        🆕 Use \`+mhw hzv [monster name]\` to get monster hitzone values!
         `
       )
       .setTimestamp()
