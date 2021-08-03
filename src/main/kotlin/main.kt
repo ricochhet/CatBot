8a169db72753cc92080e9f0794e1b0a078e5e8f8
Nathan <39814328+Yofou@users.noreply.github.com>
2021-08-03 02:58:45 +0100

    [📦] added a env config file, for bot token

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,11 +2,13 @@ import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import dev.kord.gateway.Intents
 import dev.kord.gateway.PrivilegedIntent
+import io.github.cdimascio.dotenv.dotenv
 import org.reflections.Reflections
 
 @OptIn(PrivilegedIntent::class)
 suspend fun main() {
-    val client = ExtensibleBot("NjM1OTg4MDUyMDQ0Njc3MTYw.Xa5EBw.ZE1gTdojHot--kCQpbtf5b9GYOc") {
+    val env = dotenv()
+    val client = ExtensibleBot(env["token"]) {
         intents { +Intents.all }
         slashCommands { enabled = true }
 
