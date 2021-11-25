5f47de14c17848f4ded695fa73e1c7166c6b4195
Nathan <39814328+Yofou@users.noreply.github.com>
2021-11-25 05:02:23 +0000

    [📦] Support Command

--- src/main/kotlin/extensions/support.kt
+++ src/main/kotlin/extensions/support.kt
@@ -3,8 +3,12 @@ package extensions
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
+import dev.kord.common.Color
 import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.Snowflake
+import dev.kord.rest.builder.message.create.embed
+import kotlinx.datetime.Clock
+import utils.CatBot
 
 @OptIn(KordPreview::class)
 class Support: Extension() {
@@ -16,9 +20,18 @@ class Support: Extension() {
             description = "Support server for CatBot"
             guild( Snowflake("638517240475549736") )
 
+            val invite = "https://discord.gg/p5GRCSh"
             action {
+                val kord = this@publicSlashCommand.kord
                 respond {
-                    content = "ping"
+                    embed {
+                        color = Color.CatBot
+                        title = "CatBot Support"
+                        description = "[Join the Discord]($invite)"
+
+                        timestamp = Clock.System.now()
+                        footer { text = "Support Link Request"; icon = kord.getSelf().avatar?.url  }
+                    }
                 }
             }
         }
