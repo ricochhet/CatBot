0bbc85a46e9995dd2e7640265be35995f93d728c
Nathan <39814328+Yofou@users.noreply.github.com>
2021-11-25 04:42:02 +0000

    [📦] About Command

--- src/main/kotlin/extensions/about.kt
+++ src/main/kotlin/extensions/about.kt
@@ -2,12 +2,23 @@ package extensions
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
+import dev.kord.common.Color
 import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.Snowflake
+import dev.kord.rest.builder.message.create.embed
+import kotlinx.datetime.Clock
+import utils.CatBot
 
 @OptIn(KordPreview::class)
 class About: Extension() {
     override val name = "About"
+
+    private val owner = Snowflake("838048123367456779")
+    private val devs = listOf(
+        Snowflake("264781399737892865"),
+        Snowflake("123633666550136832"),
+        Snowflake("381042290333843457"),
+    )
 
     override suspend fun setup() {
         publicSlashCommand {
@@ -16,8 +27,52 @@ class About: Extension() {
             guild( Snowflake("638517240475549736") )
 
             action {
+                val kord = this@publicSlashCommand.kord
+                val rico = kord.getUser( owner )?.tag ?: "Ricochet#9237"
+                val team = devs.mapNotNull { dev -> kord.getUser(dev)?.tag }
                 respond {
-                    content = "Ping"
+                    embed {
+                        color = Color.CatBot
+                        field {
+                            name = "Owner:"
+                            value = rico
+                        }
+
+                        field {
+                            name = "Devs:"
+                            value = team.joinToString("\n")
+                        }
+
+                        field {
+                            name = "Contributors:"
+                            value = """
+                                MechE
+                                MoonBunnie
+                                Deathcream
+                            """.trimIndent()
+                        }
+
+                        field {
+                            name = "Version:"
+                            value = "6.0.0-RC1"
+                        }
+
+                        field {
+                            name = "Changelog:"
+                            value = "Use `/changelog` to see the latest version changes."
+                        }
+
+                        field {
+                            name = "Feedback / Requests:"
+                            value = "Use `/support` to go to the support server."
+                        }
+
+                        timestamp = Clock.System.now()
+                        footer {
+                            text = "About Menu"
+                            icon = kord.getSelf().avatar?.url
+                        }
+                    }
                 }
             }
         }
