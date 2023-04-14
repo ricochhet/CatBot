0a72095e24792ebbbc6294080cba0339f00a4443
Nathan <reach@yofou.dev>
2023-04-14 05:54:51 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
     application
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
@@ -23,13 +23,14 @@ repositories {
 }
 
 dependencies {
-    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.2-SNAPSHOT")
-    implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
-    implementation("org.slf4j:slf4j-simple:1.7.35")
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.2-RC1")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
     implementation("io.ktor:ktor-client-core:1.6.7")
     implementation("io.ktor:ktor-client-cio:1.6.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("info.debatty:java-string-similarity:2.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 tasks.withType<KotlinCompile>() {
@@ -38,5 +39,5 @@ tasks.withType<KotlinCompile>() {
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    languageVersion = "1.6"
+    languageVersion = "1.8"
 }
